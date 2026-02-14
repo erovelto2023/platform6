74b0927b13ee85b490fbf4c3696a8c1343c57b2a
erovelto2023 <123208905+erovelto2023@users.noreply.github.com>
2026-02-14 18:25:24 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   title: "K Business Academy",
   description: "All-in-one educational and business-building platform.",
   icons: {
-    icon: "/icon.svg",
+    icon: "/icon.svg?v=2",
   },
 };
 
