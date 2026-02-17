fc29f48f48c369ddb104762950c93448612c5a3c
erovelto2023 <123208905+erovelto2023@users.noreply.github.com>
2026-02-17 13:38:10 -0500

    Update whiteboard-libraries.ts

--- lib/whiteboard-libraries.ts
+++ lib/whiteboard-libraries.ts
@@ -30,5 +30,173 @@ export const DEFAULT_LIBRARIES = [
     {
         name: "Storytelling",
         url: "https://libraries.excalidraw.com/libraries/drwnio/storytelling.excalidrawlib"
+    },
+    {
+        name: "Google Icons",
+        url: "https://libraries.excalidraw.com/libraries/mguidoti/google-icons.excalidrawlib"
+    },
+    {
+        name: "Developer Icons",
+        url: "https://libraries.excalidraw.com/libraries/xxxdeveloper/icons.excalidrawlib"
+    },
+    {
+        name: "Systems Design Components",
+        url: "https://libraries.excalidraw.com/libraries/arach/systems-design-components.excalidrawlib"
+    },
+    {
+        name: "Lo-Fi Wireframing Kit",
+        url: "https://libraries.excalidraw.com/libraries/spfr/lo-fi-wireframing-kit.excalidrawlib"
+    },
+    {
+        name: "Post-it",
+        url: "https://libraries.excalidraw.com/libraries/ferminrp/post-it.excalidrawlib"
+    },
+    {
+        name: "Basic Shapes",
+        url: "https://libraries.excalidraw.com/libraries/pgilfernandez/basic-shapes.excalidrawlib"
+    },
+    {
+        name: "Bubbles",
+        url: "https://libraries.excalidraw.com/libraries/ocapraro/bubbles.excalidrawlib"
+    },
+    {
+        name: "Web Kit",
+        url: "https://libraries.excalidraw.com/libraries/excacomp/web-kit.excalidrawlib"
+    },
+    {
+        name: "Gantt",
+        url: "https://libraries.excalidraw.com/libraries/ferminrp/gantt.excalidrawlib"
+    },
+    {
+        name: "Universal UI Kit",
+        url: "https://libraries.excalidraw.com/libraries/manuelernestog/universal-ui-kit.excalidrawlib"
+    },
+    {
+        name: "Emojis",
+        url: "https://libraries.excalidraw.com/libraries/anumithaapollo12/emojis.excalidrawlib"
+    },
+    {
+        name: "GCP Icons",
+        url: "https://libraries.excalidraw.com/libraries/clementbosc/gcp-icons.excalidrawlib"
+    },
+    {
+        name: "Dropdowns",
+        url: "https://libraries.excalidraw.com/libraries/h7y/dropdowns.excalidrawlib"
+    },
+    {
+        name: "Charts",
+        url: "https://libraries.excalidraw.com/libraries/g-script/charts.excalidrawlib"
+    },
+    {
+        name: "Gadgets",
+        url: "https://libraries.excalidraw.com/libraries/morgemoensch/gadgets.excalidrawlib"
+    },
+    {
+        name: "Mobile Kit",
+        url: "https://libraries.excalidraw.com/libraries/excacomp/mobile-kit.excalidrawlib"
+    },
+    {
+        name: "Polygons",
+        url: "https://libraries.excalidraw.com/libraries/lipis/polygons.excalidrawlib"
+    },
+    {
+        name: "Medias",
+        url: "https://libraries.excalidraw.com/libraries/g-script/medias.excalidrawlib"
+    },
+    {
+        name: "Awesome Slides",
+        url: "https://libraries.excalidraw.com/libraries/ferminrp/awesome-slides.excalidrawlib"
+    },
+    {
+        name: "HTML Input Elements",
+        url: "https://libraries.excalidraw.com/libraries/marwinburesch/html-input-elements.excalidrawlib"
+    },
+    {
+        name: "Office Items",
+        url: "https://libraries.excalidraw.com/libraries/m47812/office-items.excalidrawlib"
+    },
+    {
+        name: "Presentation Templates",
+        url: "https://libraries.excalidraw.com/libraries/shinkim/presentation-templates.excalidrawlib"
+    },
+    {
+        name: "Hearts",
+        url: "https://libraries.excalidraw.com/libraries/dwelle/hearts.excalidrawlib"
+    },
+    {
+        name: "Stars",
+        url: "https://libraries.excalidraw.com/libraries/lipis/stars.excalidrawlib"
+    },
+    {
+        name: "Customer Journey Map",
+        url: "https://libraries.excalidraw.com/libraries/braweria/customer-journey-map.excalidrawlib"
+    },
+    {
+        name: "Make Your Calendar",
+        url: "https://libraries.excalidraw.com/libraries/surya-teja-venteddu/make-your-calendar.excalidrawlib"
+    },
+    {
+        name: "Maps",
+        url: "https://libraries.excalidraw.com/libraries/swissarmysam/maps.excalidrawlib"
+    },
+    {
+        name: "3D Shapes",
+        url: "https://libraries.excalidraw.com/libraries/andreandreandradecosta/3d-shapes.excalidrawlib"
+    },
+    {
+        name: "Medicine",
+        url: "https://libraries.excalidraw.com/libraries/sudotachy/medicine.excalidrawlib"
+    },
+    {
+        name: "D&D Writing Icons",
+        url: "https://libraries.excalidraw.com/libraries/demondarakna/dnd-writing-icons-for-dm.excalidrawlib"
+    },
+    {
+        name: "Biology",
+        url: "https://libraries.excalidraw.com/libraries/sharathsanketh/biology.excalidrawlib"
+    },
+    {
+        name: "Stick Figures Collaboration",
+        url: "https://libraries.excalidraw.com/libraries/gianpaima/stick-figures-collaboration.excalidrawlib"
+    },
+    {
+        name: "Organization Chart",
+        url: "https://libraries.excalidraw.com/libraries/jgodoy/organization-chart.excalidrawlib"
+    },
+    {
+        name: "Domain Driven Design",
+        url: "https://libraries.excalidraw.com/libraries/cespin/domain-driven-design.excalidrawlib"
+    },
+    {
+        name: "Chess Set",
+        url: "https://libraries.excalidraw.com/libraries/pret/chess-set.excalidrawlib"
+    },
+    {
+        name: "Retail Peripherals",
+        url: "https://libraries.excalidraw.com/libraries/esteevens/retail-peripherals.excalidrawlib"
+    },
+    {
+        name: "Halloween Elements",
+        url: "https://libraries.excalidraw.com/libraries/gabrielamacakova/halloween-elements.excalidrawlib"
+    },
+    {
+        name: "Playing Cards",
+        url: "https://libraries.excalidraw.com/libraries/schiriki123/playing-cards.excalidrawlib"
+    },
+    {
+        name: "Boardgame",
+        url: "https://libraries.excalidraw.com/libraries/pocomane/boardgame.excalidrawlib"
+    },
+    {
+        name: "Head Builder Kit",
+        url: "https://libraries.excalidraw.com/libraries/pixelass/head-builder-kit-1.excalidrawlib"
+    },
+    {
+        name: "Character Kit",
+        url: "https://libraries.excalidraw.com/libraries/pixelass/character-kit-1.excalidrawlib"
+    },
+    {
+        name: "Domain Storytelling",
+        url: "https://libraries.excalidraw.com/libraries/aocgame/domainstorytelling.excalidrawlib"
     }
 ];
