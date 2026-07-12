dab87b74ace1bc7d5a984921de039cc27646288f
erovelto2023 <123208905+erovelto2023@users.noreply.github.com>
2026-07-12 14:19:30 -0400

    Protect /story-hacker routes in middleware to ensure Clerk session is populated

--- middleware.ts
+++ middleware.ts
@@ -16,6 +16,7 @@ const isProtected = createRouteMatcher([
     '/whiteboard(.*)',
     '/partner(.*)',
     '/docs(.*)',
+    '/story-hacker(.*)',
 ]);
 
 const isStudentRoute = createRouteMatcher([
